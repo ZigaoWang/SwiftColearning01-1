0a9d36e36a59d742105c19c849bc17cbf100a5d6
Zigao Wang <zigao.wang24@gmail.com>
2024-09-14 20:47:16 +0800

    Initial Commit

---
+++ SwiftColearning01-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftColearning01-1
+//
+//  Created by Zigao Wang on 9/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftColearning01-1/SwiftColearning01_1App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftColearning01_1App.swift
+//  SwiftColearning01-1
+//
+//  Created by Zigao Wang on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftColearning01_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
